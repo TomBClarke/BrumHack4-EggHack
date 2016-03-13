53c5296b9badc635a8c44587ab8f5b4a560d47e3
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-03-13 11:32:10 +0000

    working on fix

--- sql/reset.sql
+++ sql/reset.sql
@@ -42,19 +42,19 @@ CREATE TABLE found (
 INSERT INTO eggs VALUES (default, "The Start Address", "Where it all begins.", 0);
 
 -- 2
-INSERT INTO eggs VALUES (default, "localhost", "1) #3b5998 2) The song of the mountain bluebird", 1); -- The colour of facebool. The bird from the twitter logo.
+INSERT INTO eggs VALUES (default, "54.84.108.88", "1) A programmer's best friend. 2) You may enjoy some other of the author's works...", 1); -- Stack overflow. Tom's site 
 
 -- 3
-INSERT INTO eggs VALUES (default, "facebook.com", "February 2005", 1); -- The date of the founding of youtube
+INSERT INTO eggs VALUES (default, "stackoverflow.com", "Your gracious hosts.", 1); -- The university of birmingham
 
 -- 4
-INSERT INTO eggs VALUES (default, "twitter.com", "Brazil, Peru, Colombia. What do they have in common?", 1); -- Amazon rain forest
+INSERT INTO eggs VALUES (default, "tombclarke.co.uk", "I want it that way~ - Weird Al", 1); -- Ebay
 
 -- 5
-INSERT INTO eggs VALUES (default, "amazon.com", "129.42", 1); -- Part 1 of ip address
+INSERT INTO eggs VALUES (default, "ebay.co.uk", "129.42", 1); -- Part 1 of ip address
 
 -- 6
-INSERT INTO eggs VALUES (default, "youtube.com", "38.1", 1); -- Part 2 of ip address
+INSERT INTO eggs VALUES (default, "birmingham.ac.uk", "38.1", 1); -- Part 2 of ip address
 
 -- 7
 INSERT INTO eggs VALUES (default, "ibm.com", "52.450920, -1.932224 | 10:30/12/3/2016-16:30/13/3/2106", 2); -- Coordinates and timings for brumhack.
