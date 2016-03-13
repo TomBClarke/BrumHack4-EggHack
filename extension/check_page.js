44e1dfd7fe1b06ccf0d6cad61ebb9f50689daa59
cjangus <cjangus96@gmail.com>
2016-03-13 06:17:05 +0000

    Blah

--- extension/check_page.js
+++ extension/check_page.js
@@ -1,7 +1,8 @@
 var egg_page = true;
 
 if (egg_page) {
-	var currentpage = window.location.href ;
+	var currentpage = window.location.href;
+	//alert(currentpage);
 	$( document ).ready(function() {
     var elems = $('img');
 	//alert(elems.length);
@@ -9,8 +10,8 @@ if (egg_page) {
 		//alert(select);
 		//console.log(elems[select]);
 		elems[select].setAttribute("src", 'http://54.84.108.88/resources/img/EggHackGreen.png');
-		elems[select].removeAttribute("href");
-		elems[select].parent.removeAttribute("href");
+		//elems[select].removeAttribute("href");
+		//elems[select].parent.removeAttribute("href");
 		elems[select].onclick = function() {
    			window.location.href = "http://stackoverflow.com";
    		};
