78b91b7d7870c064d24da5ed12751e93018a55b2
rowanphilip <rowanphilip@googlemail.com>
2016-03-13 06:14:39 +0000

    Stuff

---
+++ sql/map.sql
@@ -0,0 +1,27 @@
+-- 1
+INSERT INTO eggs VALUES (default, "Our website", "3b5998\nThe song of the mountain bluebird", 1); -- The colour of facebool. The bird from the twitter logo.
+INSERT INTO egglinks VALUES (1, 2);
+INSERT INTO egglinks VALUES (1, 3);
+
+-- 2
+INSERT INTO eggs VALUES (default, "facebook.com", "February 2005", 1); -- The date of the founding of youtube
+INSERT INTO egglinks VALUES (2, 5);
+
+-- 3
+INSERT INTO eggs VALUES (default, "twitter.com", "Brazil, Peru, Colombia. What do they have in common?", 1); -- Amazon rain forest
+INSERT INTO egglinks VALUES (3, 4);
+
+-- 4
+INSERT INTO eggs VALUES (default, "amazon.com", "129.42", 1); -- Part 1 of ip address
+INSERT INTO egglinks VALUES (4, 6);
+
+-- 5
+INSERT INTO eggs VALUES (default, "youtube.com", "38.1", 1); -- Part 2 of ip address
+INSERT INTO egglinks VALUES (5, 6);
+
+-- 6
+INSERT INTO eggs VALUES (default, "ibm.com", "Zwys Uccuzs pih kcfgs.", 2); -- Like Google but Worse encrypted with caesar cipher
+INSERT INTO egglinks VALUES (to, from);
+
+-- 7
+INSERT INTO eggs VALUES (default, "bing.com", "Congratulations", 4);
