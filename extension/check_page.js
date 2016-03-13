9f692023887d13831ae6156b61de08bc299e8711
cjangus <cjangus96@gmail.com>
2016-03-13 04:21:41 +0000

    Prototype image injector.

--- extension/check_page.js
+++ extension/check_page.js
@@ -1,6 +1,25 @@
-// Pretend this is a web address call that returns 'true' or a number
 var egg_page = true;
 
 if (egg_page) {
-	// TODO - Inject page
+	var currentpage = window.location.href ;
+	$( document ).ready(function() {
+    var elems = $('img');
+	//alert(elems.length);
+	var select = Math.floor(Math.random() * elems.length);
+		//alert(select);
+		//console.log(elems[select]);
+		elems[select].setAttribute("src", 'http://54.84.108.88/resources/img/EggHackGreen.png');
+		elems[select].removeAttribute("href");
+		elems[select].parent.removeAttribute("href");
+		elems[select].onclick = function() {
+   			window.location.href = "http://stackoverflow.com";
+   		};
+});
+	
 }
+
+
+
+
+	
+	
